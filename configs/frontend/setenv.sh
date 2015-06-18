356c4282a35fceab95486700b6ba46d27f6e8c15
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2015-06-18 16:02:32 +0200

    Added frontend cfg files

---
+++ configs/frontend/setenv.sh
@@ -0,0 +1,3 @@
+#CATALINA_PID=$CATALINA_HOME/catalina.pid
+CATALINA_BASE=/opt/tomcat_frontend
+JAVA_OPTS="-Xms1024m -Xmx1024m -XX:MaxPermSize=256m -XX:PermSize=256m -XX:+UseConcMarkSweepGC -XX:NewSize=48m"
