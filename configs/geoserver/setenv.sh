5d9748bc4fedecddd48f9f75dea2f015e59bdfcc
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2015-06-22 09:27:25 +0200

    Added MAPFISH_PDF_FOLDER var to GeoServer env script

--- configs/geoserver/setenv.sh
+++ configs/geoserver/setenv.sh
@@ -3,7 +3,8 @@ GEOSERVER_DATA_DIR=/opt/gs_data_dir_destination_plus
 GEOSERVER_URL="http://localhost:8089/geoserver"
 IRIDE_URL="http://applogic.csi.it/pep_wsfad_policy/services/PolicyEnforcerBase"
 SIIG_URL="http://destination.geo-solutions.it/MapStore/"
-JAVA_OPTS="-server -XX:SoftRefLRUPolicyMSPerMB=36000 -Xms1024m -Xmx1024m -XX:MaxPermSize=256m -XX:PermSize=256m -XX:+UseConcMarkSweepGC -XX:NewSize=48m -Dorg.geotools.shapefile.datetime=true -DGEOSERVER_DATA_DIR=$GEOSERVER_DATA_DIR -Dorg.geotools.filter.function.simplify=true -DGEOMETRY_COLLECT_MAX_COORDINATES=50000 -DIRIDE_URL=$IRIDE_URL -DSIIG_URL=$SIIG_URL"
+MAPFISH_PDF_FOLDER="/opt/tomcat_geoserver_destination_plus/pdf"
+JAVA_OPTS="-server -XX:SoftRefLRUPolicyMSPerMB=36000 -Xms1024m -Xmx1024m -XX:MaxPermSize=256m -XX:PermSize=256m -XX:+UseConcMarkSweepGC -XX:NewSize=48m -Dorg.geotools.shapefile.datetime=true -DGEOSERVER_DATA_DIR=$GEOSERVER_DATA_DIR -Dorg.geotools.filter.function.simplify=true -DGEOMETRY_COLLECT_MAX_COORDINATES=50000 -DIRIDE_URL=$IRIDE_URL -DSIIG_URL=$SIIG_URL -DMAPFISH_PDF_FOLDER=$MAPFISH_PDF_FOLDER"
 
 export LD_LIBRARY_PATH="/usr/local/lib/"
 export GDAL_DATA="/usr/local/share/gdal/"
